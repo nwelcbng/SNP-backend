b80ebcb2b0a1b83f4c622a13db9a1b32b8ac3d9a
kid <1050109853@qq.com>
2021-08-11 22:45:56 +0800

    修改小bug

--- src/main/java/com/gdutelc/snp/cache/UserCache.java
+++ src/main/java/com/gdutelc/snp/cache/UserCache.java
@@ -4,6 +4,7 @@ import com.gdutelc.snp.dao.IUserDao;
 import com.gdutelc.snp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
@@ -17,24 +18,24 @@ public class UserCache implements IuserCache{
     @Autowired
     private IUserDao userDao;
 
-    @Cacheable(key = "result.uid")
+    @Cacheable(key = "#openid")
     @Override
     public User getUserByOpenid(String openid) {
         return userDao.getUserByOpenid(openid);
     }
 
-    @Cacheable(key = "result")
+    @Cacheable(key = "getMethodName()")
     @Override
     public String getOpenidByUid(Integer uid) {
         return userDao.getOpenidByUid(uid);
     }
 
-    @Cacheable(key = "#uid")
+    @Cacheable(key = "result.openid")
     @Override
     public User getUserByUid(Integer uid) {
         return userDao.getUserByUid(uid);
     }
-    @CachePut
+    @CacheEvict(key = "#openid")
     @Override
     public Integer updateCheckQueByOid(Integer check, String que, String openid) {
         return userDao.updateCheckQueByOid(check, que, openid);
